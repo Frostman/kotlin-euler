f52ebc127c419dfd2fe3f29c35a66da2f60d93e9
Franck Rasolo <franck.rasolo@gmail.com>
2012-03-02 00:38:52 +0000

    Renamed local functions used by sequences

--- src/euler/Sequences.kt
+++ src/euler/Sequences.kt
@@ -6,25 +6,25 @@ import java.math.BigInteger
 inline fun <T> sequence(vararg elements: T) = object : Sequence<T>() {
   val iterator: Iterator<T> = elements.iterator()
 
-  fun next(): T? { return if (iterator.hasNext) iterator.next() else null }
+  fun nextElement(): T? { return if (iterator.hasNext) iterator.next() else null }
 
-  override fun iterator(): Iterator<T> = YieldingIterator { next() }
+  override fun iterator(): Iterator<T> = YieldingIterator { nextElement() }
 }
 
 inline fun primes() = object : Sequence<Long>() {
   var number = 2.toLong()
 
-  fun prime(): Long {
+  fun nextPrime(): Long {
     while (!number.isPrime()) number++
     val result = number; number++
     return result
   }
 
-  override fun iterator(): Iterator<Long> = YieldingIterator { prime() }
+  override fun iterator(): Iterator<Long> = YieldingIterator { nextPrime() }
 }
 
 inline fun String.grouped(size: Int, iterator: CharIterator = iterator()) = object : Sequence<String>() {
-  fun group(): String? {
+  fun nextGroup(): String? {
     if (iterator.hasNext) {
       val window = StringBuilder()
       for (i in 1..size) if (iterator.hasNext) window.append(iterator.next())
@@ -32,13 +32,13 @@ inline fun String.grouped(size: Int, iterator: CharIterator = iterator()) = obje
     }
     return null
   }
-  override fun iterator(): Iterator<String> = YieldingIterator { group() }
+  override fun iterator(): Iterator<String> = YieldingIterator { nextGroup() }
 }
 
 inline fun String.sliding(size: Int, iterator: CharIterator = iterator()) = object : Sequence<String>() {
   val window = StringBuilder()
 
-  fun slide(): String? {
+  fun nextWindow(): String? {
     if (window.length() == 0) {
       for (i in 1..size) if (iterator.hasNext) window.append(iterator.next())
       return window.toString()
@@ -46,5 +46,5 @@ inline fun String.sliding(size: Int, iterator: CharIterator = iterator()) = obje
     return if (iterator.hasNext) window.deleteCharAt(0)?.append(iterator.next()).toString() else null
   }
 
-  override fun iterator(): Iterator<String> = YieldingIterator { slide() }
+  override fun iterator(): Iterator<String> = YieldingIterator { nextWindow() }
 }
