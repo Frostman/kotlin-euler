2e8b161598c43ca5349306908eeccdb3b2a10fc2
Franck Rasolo <franck.rasolo@gmail.com>
2012-03-02 03:43:45 +0000

    Refined solution to problem #10

--- src/euler/Problem-0010.kt
+++ src/euler/Problem-0010.kt
@@ -4,6 +4,7 @@ import euler.sum
 import euler.sequence.primes
 
 fun main(args : Array<String>) {
-  val result = primes().takeWhile { it < 2000000 }.toList()
-  println("the sum of all the ${result.size()} primes below two million is ${result.sum()}")
+  val limit = 2000000
+  val result = primes().takeWhile { it < limit }.toList()
+  println("the sum of all the ${result.size()} primes below $limit is ${result.sum()}")
 }
