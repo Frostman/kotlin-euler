9bceee2e9bf429cf7cbc75ccc20c7ead17baf9ed
Franck Rasolo <franck.rasolo@gmail.com>
2012-04-26 00:54:05 +0100

    A little tidy up...

--- src/euler/Problem-0014.kt
+++ src/euler/Problem-0014.kt
@@ -9,14 +9,15 @@ import java.util.List
 fun main(args : Array<String>) {
   val limit = 1000000
   val result = (1..limit).map { (n: Int) -> #(n, lengthOfSequence(n.toLong())) }.max()
-  println("the longest chain below $limit starts with ${result._1}:\n${sequence(result._1.toLong())}")
+  val chain = sequence(result._1.toLong())
+  println("the longest chain below $limit starts with ${result._1} and has ${chain.size()} elements:\n$chain")
 }
 
-fun lengthOfSequence(n: Long, length: Int = 0): Int {
+inline fun lengthOfSequence(n: Long, length: Int = 0): Int {
   return if (n == 1.toLong()) length + 1 else lengthOfSequence(if (n multipleOf 2) (n / 2) else (3 * n + 1), length + 1)
 }
 
-inline fun java.lang.Iterable<#(Int, Int)>.max() = fold(#(0, 0)) { (a, b) -> if (a._2 > b._2) a else b }
+inline fun List<#(Int, Int)>.max() = fold(#(0, 0)) { (a, b) -> if (a._2 > b._2) a else b }
 
 inline fun sequence(n: Long): List<Long> {
   if (n == 1.toLong()) return arrayList(n)
